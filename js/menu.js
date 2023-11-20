cea3e17b1bb306aa976fe2bce7e7246c22e3db0e
Francinei Silva <francineisilva7@gmail.com>
2023-11-20 20:41:28 -0300

    delete

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-// sub-menu
-var menuItem = document.querySelectorAll(".item-menu");
-
-function selectLink() {
-  this.classList.toggle("active");
-}
-
-menuItem.forEach((item) => item.addEventListener("click", selectLink));
