e8fba60c4d93aa499637c0bc2b1db5668f729ae3
Francinei Silva <francineisilva7@gmail.com>
2023-11-20 19:47:10 -0300

    Comentando

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
   // print_r($_SESSION);
   if((!isset($_SESSION['usuario']) == true) and (!isset($_SESSION['senha']) == true))
   {
-    unset($_SESSION['usuario']);
+    unset($_SESSION['usuario']); // Este código
     unset($_SESSION['senha']);
     header('Location: login.php');
   }
