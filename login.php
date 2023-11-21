02809d915bdd7e7d77d2dae323e5cb7f8e76735e
Francinei Silva <francineisilva7@gmail.com>
2023-11-20 22:08:33 -0300

    correção login

--- login.php
+++ login.php
@@ -1,28 +1,19 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/login.css">
     <title>Acesso ao Sistema</title>
 </head>
+
 <body>
     <div class="login-principal">
         <div class="login-esquerda">
-            <h1>SISTEMA DE GERENCIAMENTO<br>DE PATRIMÔNIO</h1>
+            <h1>SISTEMA DE GERENCIAMENTO <br>DE PAGAMENTO</h1>
         </div>
-        <div class="login2"></div>
-        <div class="area-login">
-            <h1>Entrar</h1>
-            <div class="textfield">
-                <input type="text" name="usuario" placeholder="Usuário" required>
-            </div>
-            <div class="textfield">
-                <input type="password" name="senha" placeholder="Senha" required>
-            </div>
-            <button class="btn-login">Entrar</button>
-        </div>
-
     </div>
 </body>
+
 </html>
