322cd6ad0f39031f4012233e53881c5e8539ff8a
Francinei Silva <francineisilva7@gmail.com>
2023-11-22 01:38:13 -0300

    Criando o css e home

---
+++ home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+    <title>Sistema de Patrimônio</title>
+</head>
+<body>
+    <aside class="menu-lateral">
+        <header class="menu-lateral-header">
+            <img class="logo-img" src="img/logo.png" alt="Logotipo Prefeitura">
+        </header>
+        
+        <nav>
+          <button>
+            <span>
+                <i class="bi bi-box-seam"></i>
+                <span>Patrimônios</span>
+            </span>
+          </button>       
+          <button>
+            <span>
+                <i class="bi bi-people-fill"></i>
+                <span>Usuários</span>
+            </span>
+          </button>       
+          <button>
+            <span>
+                <i class="bi bi-file-earmark-text"></i>
+                <span>Relatório</span>
+            </span>
+          </button>       
+        </nav>
+    </aside>
+</body>
+</html>
